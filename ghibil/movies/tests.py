283ca59a84f8a67c8513c18adc4fa5a49f235e2e
shervinmo <abtin@riseup.net>
2020-08-09 10:49:11 +0430

    add test

---
+++ ghibil/movies/tests.py
@@ -0,0 +1,15 @@
+from django.core.cache import cache
+from rest_framework.test import APITestCase
+
+
+class MovieTestCase(APITestCase):
+
+    def test_get_movies(self):
+        cache.delete_pattern("*")
+        cache_before_request = len(cache.keys("*"))
+        response = self.client.get(
+            "/api/v1/movies/", format="json",
+        )
+        self.assertEqual(response.status_code, 200)
+        cache_after_request = len(cache.keys("*"))
+        self.assertGreater(cache_after_request, cache_before_request)
