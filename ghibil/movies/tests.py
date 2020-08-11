a491e54daa768bfae0a4d888e5f30147fa0bc2ca
shervinmo <abtin@riseup.net>
2020-08-11 21:13:45 +0430

    fix api url

--- ghibil/movies/tests.py
+++ ghibil/movies/tests.py
@@ -8,7 +8,7 @@ class MovieTestCase(APITestCase):
         cache.delete_pattern("*")
         cache_before_request = len(cache.keys("*"))
         response = self.client.get(
-            "/api/v1/movies/", format="json",
+            "/movies/", format="json",
         )
         self.assertEqual(response.status_code, 200)
         cache_after_request = len(cache.keys("*"))
