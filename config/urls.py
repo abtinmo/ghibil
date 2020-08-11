3ad6e732ae565d2b8143caff5471b6dca3532c0c
shervinmo <abtin@riseup.net>
2020-08-11 21:11:06 +0430

    remove api prefix from url

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path("api/v1/", include([
+    path("", include([
         path(
             "movies/",
             include("ghibil.movies.api.v1.urls"),
